f9a88f287cbb957a19f54de0d44998be1ad5274a
Syed Kounain Abbas Rizvi <abbasmashaddy72@gmail.com>
2022-11-25 11:13:38 +0530

    theme commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import liveReload from "vite-plugin-live-reload";
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+        liveReload("./app/Http/**/*.php"),
+        laravel([
+            "resources/css/frontend/app.css",
+            "resources/js/frontend/app.js",
+        ]),
+        laravel([
+            "resources/css/backend/app.css",
+            "resources/js/backend/app.js",
+        ]),
+        {
+            name: "blade",
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith(".blade.php")) {
+                    server.ws.send({
+                        type: "full-reload",
+                        path: "*",
+                    });
+                }
+            },
+        },
     ],
 });
